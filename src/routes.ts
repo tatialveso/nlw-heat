3dc7567ba0303f7330b7d6e7f0a2ff33178cb6c4
tatialveso <tatiialveso@gmail.com>
2021-10-21 13:48:42 -0300

    :sparkles: get user

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./controller/AuthenticateUserController";
 import { CreateMessageController } from "./controller/CreateMessageController";
 import { GetLast3MessageController } from "./controller/GetLast3MessagesController";
+import { ProfileUserController } from "./controller/ProfileUserController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
 
 router.get("/messages/last3", new GetLast3MessageController().handle);
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
 
 router.post("/authenticate", new AuthenticateUserController().handle);
 router.post("/messages", ensureAuthenticated, new CreateMessageController().handle);
