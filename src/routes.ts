91e90b1a29eded64e9399e11f17c62fc8eed52f0
tatialveso <tatiialveso@gmail.com>
2021-10-21 13:34:11 -0300

    :sparkles: create find messages

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controller/AuthenticateUserController";
 import { CreateMessageController } from "./controller/CreateMessageController";
+import { GetLast3MessageController } from "./controller/GetLast3MessagesController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
+
+router.get("/messages/last3", new GetLast3MessageController().handle);
 
 router.post("/authenticate", new AuthenticateUserController().handle);
 router.post("/messages", ensureAuthenticated, new CreateMessageController().handle);
